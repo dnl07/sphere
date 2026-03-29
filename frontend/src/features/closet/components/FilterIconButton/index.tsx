c0e59409b3b48b0d518a187c785bafd8dc9d149d
dnl07 <daniel.baranovskiy07@gmail.com>
2026-03-30 01:10:28 +0200

    fix: filter button activating filter menu

--- frontend/src/features/closet/components/FilterIconButton/index.tsx
+++ frontend/src/features/closet/components/FilterIconButton/index.tsx
@@ -4,12 +4,12 @@ import SearchIcon from "../../../../shared/components/ui/icons/SearchIcon";
 import FilterIcon from "../../../../shared/components/ui/icons/FilterIcon";
 
 type Props = {
-    onExpand: () => void,
+    filterMenuOpen: boolean
     onFilter: () => void
 }
 
-const FilterIconButton = ({ onExpand, onFilter }: Props) => {
-    const [ onExpanded, setOnExpanded ] = useState(false);
+const FilterIconButton = ({ filterMenuOpen, onFilter }: Props) => {
+    const [ isExpanded, setIsExpanded ] = useState(false);
     const [searchParams, setSearchParams] = useSearchParams();
     
     const [query, setQuery] = useState<string | null>(null)
@@ -34,11 +34,24 @@ const FilterIconButton = ({ onExpand, onFilter }: Props) => {
         addQueryToUrl();
     }, [query]);
 
+    const toggleExpansion = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const toggleFilter = () => {
+        if (isExpanded && filterMenuOpen) {
+            onFilter();
+        } else if (!isExpanded && !filterMenuOpen) {
+            onFilter();
+        }
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div className={`fixed bottom-1 md:bottom-3 z-30 max-w-6xl w-full flex px-3 `}>
-            <div className={`bg-black p-3 flex gap-3 transition-all ease-in-out duration-200 ${onExpanded ? "w-full" : "w-39"}`}>
-                <div className={`flex justify-center items-center text-white text-xl border-white border-2 h-15 p-2 transition-all ease-in-out duration-200 cursor-pointer ${onExpanded ? "w-full" : "w-15"}`} onClick={() => setOnExpanded(!onExpanded)}>
-                    {onExpanded ? 
+        <div className={`fixed bottom-1 md:bottom-3 z-30 max-w-6xl w-full flex px-3 cursor-pointer`}  onClick={toggleExpansion}>
+            <div className={`bg-black p-3 flex gap-3 transition-all ease-in-out duration-200 ${isExpanded ? "w-full" : "w-39"}`}>
+                <div className={`flex justify-center items-center text-white text-xl border-white border-2 h-15 p-2 transition-all ease-in-out duration-200  ${isExpanded ? "w-full" : "w-15"}`}>
+                    {isExpanded ? 
                       <input type="text" name="search"
                         className="text-white outline-none w-full pl-3"
                         placeholder="Search..."
@@ -50,7 +63,7 @@ const FilterIconButton = ({ onExpand, onFilter }: Props) => {
                         <SearchIcon className="w-8 h-8"/>             
                 }
                 </div>
-                <button className="text-white flex justify-center items-center border-white border-2 h-15 p-2 aspect-square cursor-pointer" onClick={() => {onFilter(); setOnExpanded(!onExpanded)}}>
+                <button className="text-white flex justify-center items-center border-white border-2 h-15 p-2 aspect-square cursor-pointer" onClick={toggleFilter}>
                     <FilterIcon className="w-8 h-8" strokeWidth={1.25}/>
                 </button>
             </div>
--- frontend/src/features/closet/index.tsx
+++ frontend/src/features/closet/index.tsx
@@ -8,17 +8,15 @@ import FilterMenu from "./components/FilterMenu";
 const Closet = () => {
     const [ filterMenuOpen, setFilterMenuOpen ] = useState(false);
 
-
-
     return (
-        <PageWrapper title="Closet">
+        <PageWrapper>
             <ClosetProvider>
                 <div className=" w-full border-b mb-2">
                     <h1 className="text-3xl align-self-end px-2 py-1">Closet</h1>
                 </div>
                 <ClothingGallery />
                 <FilterMenu open={filterMenuOpen} closeFilter={() => setFilterMenuOpen(false)}/>
-                <FilterIconButton onExpand={() => {}} onFilter={() => setFilterMenuOpen(!filterMenuOpen)}/>
+                <FilterIconButton filterMenuOpen={filterMenuOpen} onFilter={() => setFilterMenuOpen(!filterMenuOpen)}/>
             </ClosetProvider>
         </PageWrapper>
     );
